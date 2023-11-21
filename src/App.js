281fac26ac1ab4af9b9abf6ddf782a79b5c9a9bb
pavan <pavanat24official@gmail.com>
2023-11-21 18:03:47 +0530

    register

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Register from './components/Register';
+import Home from './components/Home';
 function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
