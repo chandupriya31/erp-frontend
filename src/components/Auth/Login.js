733b222ef6cab17bcae00d52d4711fed4dffe5ef
pavan <pavanat24official@gmail.com>
2023-11-22 17:04:03 +0530

    login

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,61 +1,110 @@
-import { useFormik } from 'formik'
-import axios from '../../config/axios'
-import { useNavigate } from 'react-router-dom'
-import { useContext } from 'react'
-import * as Yup from 'yup'
-import { UserContext } from '../../App'
-import { useNavigation } from 'react-router-dom'
+import { useFormik } from 'formik';
+import axios from '../../config/axios';
+import { useNavigate, Link } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import * as Yup from 'yup';
+import { UserContext } from '../../App';
+
 export default function Login() {
-   const { userDispatch } = useContext(UserContext)
-   const navigate = useNavigate()
+   const { userDispatch } = useContext(UserContext);
+   const [serverError, setServerError] = useState([]);
+   const navigate = useNavigate();
+
+   const registerValidation = Yup.object().shape({
+      email: Yup.string()
+         .required('Email is required')
+         .email('Please enter a valid email'),
+      password: Yup.string()
+         .required('Password is required')
+         .min(8, 'Password must be at least 8 characters')
+   });
+
    const formik = useFormik({
       initialValues: {
          email: '',
          password: ''
       },
+      validationSchema: registerValidation,
+      validateOnChange: false,
       onSubmit: async (formData) => {
-         const data = {
-            email: formData.email,
-            password: formData.password
-         }
-         // console.log(data)
          try {
-            const response = await axios.post('/api/login', data)
-            console.log(response.data)
-            localStorage.setItem('token', response.data.token)
+            const response = await axios.post('/api/login', formData);
+            localStorage.setItem('token', response.data.token);
             const profile = await axios.get('/api/getprofile', {
                headers: {
                   Authorization: localStorage.getItem('token')
                }
-            })
-            const user = profile.data
-            console.log('user', user);
-            const companyuser = profile.data.user
-            console.log(companyuser, 'com-user')
-            if (user.role == 'customer') {
-               userDispatch({ type: 'USER_LOGIN', payload: user })
-               navigate('/customer')
+            });
+            const user = profile.data;
+            const companyUser = profile.data.user;
+
+            if (user.role === 'customer') {
+               userDispatch({ type: 'USER_LOGIN', payload: user });
+               navigate('/customer');
             }
-            if (user.user.role == 'companyAdmin') {
-               userDispatch({ type: 'USER_LOGIN', payload: companyuser })
-               navigate('/companyContainer')
+            if (user.user.role === 'companyAdmin') {
+               userDispatch({ type: 'USER_LOGIN', payload: companyUser });
+               navigate('/companyContainer');
             }
          } catch (e) {
-            console.log(e)
+            setServerError(e.response.data.errors);
          }
       }
-   })
-
+   });
    return (
-      <div>
-         <form onSubmit={formik.handleSubmit}>
-            <label>enter email</label><br />
-            <input type="email" value={formik.values.email} onChange={formik.handleChange} name="email" /><br />
-            <label>enter password</label><br />
-            <input type="password" value={formik.values.password} onChange={formik.handleChange} name="password" /><br />
-            <input type="submit" />
-         </form>
-      </div>
+      <div className="container">
+         <div className="row justify-content-center">
+            <div className="col-md-6">
+               <div className="card w-75 ">
+                  <div className="card-body d-flex flex-column ">
+                     <h5 className="card-title text-center mb-4">Login</h5>
+                     <form onSubmit={formik.handleSubmit}>
+                        <div className="mb-3">
+                           <label htmlFor="email" className="form-label">Enter Email</label>
+                           <input
+                              type="email"
+                              className="form-control"
+                              id="email"
+                              value={formik.values.email}
+                              onChange={formik.handleChange}
+                              name="email"
+                           />
+                           <div className="text-danger">{formik.errors.email}</div>
+                        </div>
 
-   )
+                        <div className="mb-3">
+                           <label htmlFor="password" className="form-label">Enter Password</label>
+                           <input
+                              type="password"
+                              className="form-control"
+                              id="password"
+                              value={formik.values.password}
+                              onChange={formik.handleChange}
+                              name="password"
+                           />
+                           <div className="text-danger">{formik.errors.password}</div>
+                        </div>
+
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                        <h5 className="card-title text-center mt-4">Register here - <Link to='/register'>SignUp</Link></h5>
+                     </form>
+                  </div>
+               </div>
+            </div>
+            {/* Display server errors on the right side */}
+            <div className="col-md-6">
+               {serverError.length > 0 && (
+                  <div className="server-errors mt-3">
+                     {serverError.map((ele, index) => (
+                        <div key={index} className="alert alert-danger" style={{ width: '200px' }} role="alert">
+                           <b>Server Error</b><br />
+                           {ele.msg}
+                        </div>
+                     ))}
+                  </div>
+               )}
+            </div>
+         </div>
+      </div>
+   );
 }
