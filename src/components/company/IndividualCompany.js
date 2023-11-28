06eb5c90aae87eb39c65ca2605be52617c1f9085
chandupriya <priyadavuluru@gmail.com>
2023-11-28 10:29:44 +0530

    map component

--- src/components/company/IndividualCompany.js
+++ src/components/company/IndividualCompany.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { UserContext } from "../../App";
 import IndividualIntervalsExample from "../caroselhome";
@@ -11,20 +11,26 @@ import { BiPhone } from "react-icons/bi";
 function IndividualCompany() {
   const { id } = useParams();
   const { userState } = useContext(UserContext);
+  const [mapInit, setMapInit] = useState(false);
   const company = userState.companylist.find((ele) => ele._id === id);
 
   useEffect(() => {
-    if (company && company.contactdetails.address.lattitude && company.contactdetails.address.longitude) {
-      const map = L.map("map").setView(
-        [company.contactdetails.address.lattitude, company.contactdetails.address.longitude],
-        13
-      );
-      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: "© OpenStreetMap contributors",
-      }).addTo(map);
-      L.marker([company.contactdetails.address.lattitude, company.contactdetails.address.longitude]).addTo(map);
+    if (company && company.contactdetails.address.lattitude && company.contactdetails.address.longitude && !mapInit) {
+      try {
+        const mapcomponent = L.map("map-style").setView(
+          [company.contactdetails.address.lattitude, company.contactdetails.address.longitude],
+          13
+        );
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", 
+        ).addTo(mapcomponent)
+        
+        L.marker([company.contactdetails.address.lattitude, company.contactdetails.address.longitude]).addTo(mapcomponent);
+        setMapInit(true);
+      } catch (error) {
+        console.error('Error creating map or marker:', error);
+      }
     }
-  }, [company]);
+  }, [company, mapInit]);
 
   if (!company) {
     return <p>Loading...</p>;
@@ -43,14 +49,14 @@ function IndividualCompany() {
           <p className="text-muted"><strong>{company.details.vision}</strong></p>
           <h4 className="text-primary mt-4">Company's Mission</h4>
           <p className="text-muted"><strong>{company.details.mission}</strong></p>
-          <b><hr /></b>
+          <hr style={{ height: '5px', color: "black", backgroundColor: "black", border: 'none' }} />
           <div className="mt-4" style={{ backgroundColor: "black", color: "white", padding: "20px" }}>
             <h6>Contact Details</h6>
             <div className="d-flex align-items-center">
               <i className="fas fa-map-marker-alt me-2 text-primary"></i>
               <p><BiLocationPlus /> Address - <strong>{company.contactdetails.address.name}</strong></p>
             </div>
-            <div id="map" style={{ height: "200px", marginTop: "10px", width: "40%", marginLeft: '30px' }}></div>
+            <div id="map-style" style={{ height: "200px", marginTop: "10px", width: "100%" }}></div>
             <div className="d-flex align-items-center">
               <i className="fas fa-envelope me-2 text-primary"></i>
               <p><BsEnvelope /> Email - <strong>{company.contactdetails.email}</strong></p>
